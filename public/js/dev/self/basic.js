2d02331399228d1f9d407ed34037e09d2c1aceb8
mrhop <233402923@qq.com>
2016-07-14 15:12:29 +0800

    commit

--- public/js/dev/self/basic.js
+++ public/js/dev/self/basic.js
@@ -497,7 +497,7 @@ $(document).ready(function () {
             slideshow: true,
             animation: "slide",
             before: function (slider) {
-                $("#page6 .first-container .control-div div.circle.direct").css("left", (slider.animatingTo * 14.28 + 7) + "%");
+                $("#page6 .first-container .control-div div.circle.direct").css("left", (slider.animatingTo * 14.28 + 7.1) + "%");
             }
         });
     }
